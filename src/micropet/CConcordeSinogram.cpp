17b2da70a2108d6b0aec6034577840f1db9f2d26
Hagen Mölle <h.moelle@hzdr.de>
2004-08-31 13:39:33 +0000

    - implemented the get method to get appropriate data of the desired frame

--- src/micropet/CConcordeSinogram.cpp
+++ src/micropet/CConcordeSinogram.cpp
@@ -5,6 +5,7 @@
 #include "../include/debug.h"
 
 #include <qdatastream.h>
+#include <qfile.h>
 
 CConcordeSinogram::CConcordeSinogram(QString file):CConcordeFile(file)
 {
@@ -21,40 +22,48 @@ CConcordeSinogram::~CConcordeSinogram()
 QByteArray* CConcordeSinogram::get(short frame, short plane, 
 			short gate, short bed, short data)
 {
-	/*
 	if(CMedIOData::getHeader()->rtti() == CMedIOHeader::ConcordeMicropet)
 	{
 		CHeaderConcorde* head = (CHeaderConcorde*)CMedIOData::getHeader();
 		
-		QDataStream data_stream(CMedIOData::getFile());
-		
-		//get byte order from header and set it in datastream
-		if(head->getDataType() == "0")
+		QFile pfile(CMedIOData::getFile());
+		if(pfile.open(IO_ReadOnly))
 		{
-			return NULL;
-		}
+			QDataStream data_stream(&pfile);
 		
-		if((head->getDataType()=="2" |
-			head->getDataType()=="3") |
-			head->getDataType()=="4")
-		{
-			data_stream.setByteOrder(QDataStream::LittleEndian);
+			//get byte order from header and set it in datastream
+			if(head->getDataType() == "0")
+			{
+				return NULL;
+			}
+			
+			if((head->getDataType()=="2" |
+				head->getDataType()=="3") |
+				head->getDataType()=="4")
+			{
+				data_stream.setByteOrder(QDataStream::LittleEndian);
+			}
+			else
+			{
+				data_stream.setByteOrder(QDataStream::BigEndian);
+			}
+		
+			char * data = new char[head->getFrameSize()];
+			data_stream.readRawBytes(data,head->getFrameSize());
+			
+			//close file
+			pfile.close();
+			
+			QByteArray* data_bytes = new QByteArray();
+			data_bytes->setRawData(data,head->getFrameSize()/2);
+		
+			return data_bytes;
 		}
 		else
-		{
-			data_stream.setByteOrder(QDataStream::BigEndian);
-		}
-		
-		char * data = new char[head->getFrameSize()];
-		data_stream.readRawBytes(data,head->getFrameSize());
-		
-		QByteArray* data_bytes = new QByteArray();
-		data_bytes->setRawData(data,head->getFrameSize()/2);
-		
-		return data_bytes;
+			return NULL;
 	}
-	*/
-	return NULL;
+	else 
+		return NULL;
 }
 
 CMedIOData::Format CConcordeSinogram::rtti()
