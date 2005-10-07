3315f4f1072309b484dd340f46d7679c3339a189
Hagen Mölle <h.moelle@hzdr.de>
2005-10-07 13:42:16 +0000

    - new qt profile file

--- examples/mp2ecat/mp2ecat.cpp
+++ examples/mp2ecat/mp2ecat.cpp
@@ -17,99 +17,128 @@ using namespace std;
 
 int main( int argc, char ** argv ) 
 {
+	int result = 0;
 	if(argc == 3)
     	{
-		QByteArray* data = new QByteArray();
 		QString name(argv[1]);
 		QString StoreFileName(argv[2]);
+		
+		cout << "Opening Image file: " << argv[1] << endl;
+		
+		//create new CMedIOData object from file
 		CMedIOData* ImageVolume = CMedIODataFactory::create(name);
 		if(ImageVolume == NULL)
-			return 0;
-		ImageVolume->open(QIODevice::ReadOnly);
-		CHeaderConcorde* head = (CHeaderConcorde*)ImageVolume->header();
-		if(head == NULL)
-			return 0;
-
-		CECATFile e7image(StoreFileName,CECATMainHeader::ECAT7_Volume16);
-		e7image.open(QIODevice::WriteOnly);	
-		CECAT7MainHeader* e7_header = (CECAT7MainHeader*)e7image.createEmptyMainHeader();
-		
-		*static_cast<CMedIOHeader*>(e7_header) = *static_cast<CMedIOHeader*>(head);
-	
-		for(int i = 0; i < head->numframes(); i++)
 		{
-			data = ((CConcordeImage*)ImageVolume)->getMatrix(i+1,0,0,0,0);
-			
-			unsigned int framesize = head->getImageFrameSize();
-			float* b = (float*)data->data();
-			
-			char* byte_image = new char[framesize/2];
-			
-			float max = 0.0F;
-			float min = 10000.0F;
-			float range;
-			for(unsigned int j = 0; j < framesize/4; j++, b++)
-			{
-				if(max < *b)
-					max = *b;
-				if(min > *b)
-					min = *b;
-			}
-			
-			if(fabs(max) > fabs(min))
-			{
-				range = fabs(max);
-			}
-			else
-			{
-				range = fabs(min);
-			}
-			
-			float scale_factor = range/32767.0;
-			b = (float*)data->data();
-			short* l = (short*)byte_image;
-			for(unsigned int j = 0; j < framesize/4; j++, b++,l++)
-			{
-				*l = (short) floor(*b*32767.0/range);
-				
-			}
-		
-			cout << "Scalefactor" << scale_factor << endl;
-			cout << head->frame(i+1)->scalefactor() << endl;
-			
-			delete data;
-			data = new QByteArray(byte_image,framesize/2);
-			delete byte_image;
-			
-			CECAT7SubHeaderImage* e7_subheader;
-			e7_subheader = (CECAT7SubHeaderImage*)e7image.createEmptySubHeader();
-			//*e7_subheader = *head;
-
-			e7_subheader->setScale_Factor(head->frame(i+1)->scalefactor()*scale_factor);
-			if(fabs(max) > fabs(min))
-			{
-				e7_subheader->setImage_Min((short)ceil(min*scale_factor));
-				e7_subheader->setImage_Max(32767);
-			}
-			else
-			{
-				e7_subheader->setImage_Max((short)ceil(min*scale_factor));
-				e7_subheader->setImage_Min(-32768);
-			}
-			e7_subheader->setFrame_Duration((int)(head->frame(i+1)->frameduration()*1000.0));
-			e7_subheader->setFrame_Start_Time((int)(head->frame(i+1)->framestart()*1000.0));
-			e7_subheader->setDecay_Corr_Fctr(head->frame(i+1)->decaycorrection());
-			e7image.writeSubHeader(*e7_subheader, i+1);
-			e7image.writeMatrix(*data,i+1);
+			cout << "Image source file does not exist." << endl;
+			result = 1;
 		}
-		ImageVolume->close();
-		e7image.writeMainHeader(*e7_header);
-		e7image.close();
+		else
+		{
+			if(!ImageVolume->open(QIODevice::ReadOnly) || !(ImageVolume->rtti() == CMedIOData::ConcordeMicropet))
+				result = 1;
+			else
+			{
+				CHeaderConcorde* head = NULL;
+				if(!((CConcordeFile*)ImageVolume)->readMainHeader(head) || !(head->fileType() == CHeaderConcorde::Image))
+					result = 1;
+				else
+				{
+					CECATFile e7image(StoreFileName,CECATMainHeader::ECAT7_Volume16);
+					e7image.open(QIODevice::WriteOnly);	
+					CECAT7MainHeader* e7_header = (CECAT7MainHeader*)e7image.createEmptyMainHeader();
+					
+					*static_cast<CMedIOHeader*>(e7_header) = *static_cast<CMedIOHeader*>(head);
+				
+					for(int i = 0; i < head->totalFrames(); i++)
+					{
+						QByteArray* data = NULL;
+						CHeaderConcordeFrame* subHeader = NULL;
+						if(!((CConcordeFile*)ImageVolume)->readSubHeader(subHeader, i+1) || !((CConcordeFile*)ImageVolume)->readMatrix(data, i+1))
+						{
+							result = 1;
+							delete data;
+							delete subHeader;
+							break;
+						}
+						else
+						{	
+							unsigned int framesize = head->getImageFrameSize();
+							float* b = (float*)data->data();
+							
+							char* byte_image = new char[framesize/2];
+							
+							float max = 0.0F;
+							float min = 10000.0F;
+							float range;
+							for(unsigned int j = 0; j < framesize/4; j++, b++)
+							{
+								if(max < *b)
+									max = *b;
+								if(min > *b)
+									min = *b;
+							}
+							
+							if(fabs(max) > fabs(min))
+							{
+								range = fabs(max);
+							}
+							else
+							{
+								range = fabs(min);
+							}
+							
+							float scale_factor = range/32767.0;
+							b = (float*)data->data();
+							short* l = (short*)byte_image;
+							for(unsigned int j = 0; j < framesize/4; j++, b++,l++)
+							{
+								*l = (short) floor(*b*32767.0/range);
+								
+							}
+						
+							cout << "Scalefactor: " << scale_factor << endl;
+							cout << subHeader->scaleFactor() << endl;
+							
+							delete data;
+							data = new QByteArray(byte_image,framesize/2);
+							delete byte_image;
+							
+							CECAT7SubHeaderImage* e7_subheader;
+							e7_subheader = (CECAT7SubHeaderImage*)e7image.createEmptySubHeader();
+							
+							//*static_cast<CMedIOHeader*>(e7_subheader) = *static_cast<CMedIOHeader*>(subHeader);
+				
+							e7_subheader->setScale_Factor(subHeader->scaleFactor()*scale_factor);
+							if(fabs(max) > fabs(min))
+							{
+								e7_subheader->setImage_Min((short)ceil(min*scale_factor));
+								e7_subheader->setImage_Max(32767);
+							}
+							else
+							{
+								e7_subheader->setImage_Max((short)ceil(min*scale_factor));
+								e7_subheader->setImage_Min(-32768);
+							}
+							e7_subheader->setFrame_Duration((int)(subHeader->frameDuration()*1000.0));
+							e7_subheader->setFrame_Start_Time((int)(subHeader->frameStart()*1000.0));
+							e7_subheader->setDecay_Corr_Fctr(subHeader->decayCorrection());
+							e7image.writeSubHeader(*e7_subheader, i+1);
+							e7image.writeMatrix(*data,i+1);
+							delete data;
+							delete subHeader;
+						}
+					}
+					ImageVolume->close();
+					e7image.writeMainHeader(*e7_header);
+					e7image.close();	
+				}
+			}
+		}
 	}
 	else
 	{
 		cout << "no input or outputfilename given" << endl;			
-		return 0;
+		result = 1;
 	}
-	return 0;
+	return result;
 }
