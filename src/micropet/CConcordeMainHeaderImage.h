078951321e553851153aec1f88ace161335c0d57
Jens Maus <mail@jens-maus.de>
2005-10-10 11:38:47 +0000

    This commit was manufactured by cvs2svn to create branch 'QT3_EDITION'.

---
+++ src/micropet/CConcordeMainHeaderImage.h
@@ -0,0 +1,19 @@
+//! @file CConcordeMainHeaderImage.h
+//! @author Hagen Moelle
+//! @date 09/10/2005
+//! @brief contains definition of the class CConcordeMainHeaderImage
+
+#ifndef CCONCORDEMAINHEADERIMAGE_H
+#define CCONCORDEMAINHEADERIMAGE_H
+
+#include "CConcordeMainHeader.h"
+
+class CConcordeMainHeaderImage : public CConcordeMainHeader
+{
+	public:
+		CConcordeMainHeaderImage(QString File) : CConcordeMainHeader(File){};
+		CConcordeMainHeaderImage(CConcordeFile* file) : CConcordeMainHeader(file){};
+		unsigned int frameSize();
+};
+
+#endif
