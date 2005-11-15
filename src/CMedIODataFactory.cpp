ce504852c96ff4a876c6dfe945556831eb4d654f
Hagen Mölle <h.moelle@hzdr.de>
2005-11-15 14:08:18 +0000

    - fixed syntax error

--- src/CMedIODataFactory.cpp
+++ src/CMedIODataFactory.cpp
@@ -67,11 +67,11 @@ CMedIOData* CMedIODataFactory::create(const QString& fileName)
 	CMedIOData* mData;
 
 	// here we check for our different main data formats 
-	if(mData = CECATFile::createFromFile(fileName)
+	if(mData = CECATFile::createFromFile(fileName))
 	{
 		D("ECAT format found");
 	}
-	else if(mData = CConcordeFile::createFromFile(fileName)
+	else if(mData = CConcordeFile::createFromFile(fileName))
 	{
 		D("Concorde microPET format found");
 	}
