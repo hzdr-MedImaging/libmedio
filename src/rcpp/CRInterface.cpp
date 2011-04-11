396dcd8acd1ce4f2f2c3509cbf16ef747109ffba
Stan Domula <s.domula@hzdr.de>
2011-04-11 16:04:11 +0000

    - remove method asRList() from CRInterface class and put its functionality into readEcat()

--- src/rcpp/CRInterface.cpp
+++ src/rcpp/CRInterface.cpp
@@ -15,7 +15,7 @@ RcppExport SEXP readEcat(SEXP vfile,
                          SEXP numberOfCols,
                          SEXP numberOfPlanes)
 {
-  Rcpp::List l;
+  Rcpp::List rList;
 
   QString filename;
   vector<short> nv;
@@ -46,7 +46,71 @@ RcppExport SEXP readEcat(SEXP vfile,
     if(inputFile.open(QIODevice::ReadOnly) &&
        (inputFile.format() != CECATFile::Undefined) )
     {
-      l = inputFile.asRList(nv, rows, cols, planes);
+      Rcpp::List mhead;
+      Rcpp::List subHeader;
+
+      cout << "lenght of rows: " << rows.size() << endl;
+
+      if(inputFile.readMainHeader(mhead) == true)
+      {
+        int num_frames = static_cast<int>(mhead("num_frames"));
+        int num_gates = static_cast<int>(mhead("num_gates"));
+        int num_bed_pos = static_cast<int>(mhead("num_bed_pos")) + 1;
+        int num_volumes = 0;
+
+        if((num_frames >= 1) && (num_gates <= 1) && (num_bed_pos <= 1))
+        {
+          num_volumes = num_frames;
+        }
+
+        Rcpp::NumericVector* matrixData = NULL;
+
+        for(short frame = 1; frame <= num_frames; frame++)
+        {
+
+          if(inputFile.readMatrix(matrixData, subHeader, frame/*, 1, gate, bed, data*/) == true)
+          {
+            QString name("f");
+            name.append(QString::number(frame));
+
+            if(rows.size() != 0)
+            {
+              vector<short>::iterator it;
+              for(it = rows.begin(); it != rows.end(); ++it)
+                cout <<  *it << endl;
+            }
+
+            rList.push_back(*matrixData, name.toAscii().constData());
+
+            rList.attr("class") = "Ecat";
+            rList.attr("mhead") = mhead;
+
+            Rcpp::List::iterator it = rList.begin();
+            Rcpp::NumericVector frame_midpoint;
+            for(; it != rList.end(); it++)
+            {
+              unsigned int frame_duration = static_cast<unsigned int>(subHeader("frame_duration"));
+              unsigned int frame_start = static_cast<unsigned int>(subHeader("frame_start"));
+              double fm = frame_start + frame_duration / 2;
+              frame_midpoint.push_back(fm);
+            }
+
+            rList.attr("frame_midpoint") = frame_midpoint;
+          }
+          else
+          {
+            cerr << "ERROR: can't read matrix data" << endl;
+          }
+
+          if(matrixData != NULL)
+            delete matrixData;
+        }
+
+      }
+      else
+      {
+        cerr << "ERROR: can't read main header" << endl;
+      }
     }
     else
     {
@@ -59,28 +123,12 @@ RcppExport SEXP readEcat(SEXP vfile,
     forward_exception_to_r(e);
   }
  
-  return l;
+  return rList;
 }
-
-
-class CRInterfacePrivate
-{
-  public:
-    vector<short> m_Volumes;
-    vector<short> m_Rows;
-    vector<short> m_Cols;
-    vector<short> m_Planes;
-};
 
 CRInterface::CRInterface(const QString& filename, CECATMainHeader::Type fileType )
   : CECATFile(filename, fileType)
 {
-  m_pData = new CRInterfacePrivate();
-}
-
-CRInterface::~CRInterface()
-{
-  delete m_pData;
 }
 
 bool CRInterface::readMainHeader(Rcpp::List& mainHeader)
@@ -152,7 +200,6 @@ bool CRInterface::readMatrix(Rcpp::NumericVector*& matrixData, Rcpp::List& subHe
     unsigned int frame_duration = sHeadImage->frame_Duration() / 1000; // [s]
     unsigned int frame_start = sHeadImage->frame_Start_Time() / 1000;  // [s]
 
-#warning TODO: implement rows, cols and planes selection
     short minRow = 1;
     short maxRow = y_dimension;
     short minCol = 1;
@@ -238,59 +285,4 @@ bool CRInterface::readMatrix(Rcpp::NumericVector*& matrixData, Rcpp::List& subHe
     delete sHead;
 
   return bResult;
-}
-
-Rcpp::List CRInterface::asRList(vector<short>& nv,
-                               vector<short>& rows,
-                               vector<short>& cols,
-                               vector<short>& planes)
-{
-  bool bResult = true;
-  Rcpp::List list;
-  Rcpp::List mhead;
-  Rcpp::List subHeader;
-
-  if(readMainHeader(mhead) == true)
-  {
-    Rcpp::NumericVector* matrixData = NULL;
-
-    for(short frame = 1; frame <= numFrames(); frame++)
-    {
-      short plane = 1;
-      if(readMatrix(matrixData, subHeader, frame/*, plane, gate, bed, data*/) == true)
-      {
-        QString name("f");
-        name.append(QString::number(frame));
-        list.push_back(*matrixData, name.toAscii().constData());
-
-        list.attr("class") = "Ecat";
-        list.attr("mhead") = mhead;
-
-        Rcpp::List::iterator it = list.begin();
-        Rcpp::NumericVector frame_midpoint;
-        for(; it != list.end(); it++)
-        {
-          unsigned int frame_duration = static_cast<unsigned int>(subHeader("frame_duration"));
-          unsigned int frame_start = static_cast<unsigned int>(subHeader("frame_start"));
-          double fm = frame_start + frame_duration / 2;
-          frame_midpoint.push_back(fm);
-        }
-
-        list.attr("frame_midpoint") = frame_midpoint;
-      }
-      else
-      {
-        cerr << "ERROR: can't read matrix data" << endl;
-        bResult = false;
-      }
-
-      if(matrixData != NULL)
-        delete matrixData;
-    }
-
-  }
-  else
-    bResult = false;
-
-  return list;
 }
--- src/rcpp/CRInterface.h
+++ src/rcpp/CRInterface.h
@@ -14,28 +14,16 @@ RcppExport SEXP readEcat(SEXP vfile,
                          SEXP numberOfCols = NULL,
                          SEXP numberOfPlanes = NULL);
 
-class CRInterfacePrivate;
-
 class CRInterface : public CECATFile
 {
 public:
   CRInterface(const QString& filename,
              CECATMainHeader::Type fileType = CECATMainHeader::Unknown);
 
-  ~CRInterface();
-
   operator SEXP();
 
   bool readMainHeader(Rcpp::List& mainHeader);
   bool readMatrix(Rcpp::NumericVector*& matrixData, Rcpp::List& subHeader,
                   short frame, short plane=1, short gate=1, short bed=0, short data=0);
-
-  Rcpp::List asRList(std::vector<short>& volumes,
-                     std::vector<short>& rows,
-                     std::vector<short>& cols,
-                     std::vector<short>& planes);
-
-private:
-  CRInterfacePrivate *m_pData;
 };
 #endif
