f4b48e78a7506b918dc85576f1567d9064ff9143
Stan Domula <s.domula@hzdr.de>
2011-04-12 09:41:21 +0000

    - added RInterface files, to provide functions which can be called from R

--- src/rcpp/CRECATFile.h
+++ src/rcpp/CRECATFile.h
@@ -7,20 +7,11 @@
 
 #include <CECATFile.h>
 
-// this function can be called from R using the .Call interface
-RcppExport SEXP readEcat(SEXP vfile,
-                         SEXP numberOfVolumes = NULL,
-                         SEXP numberOfRows = NULL,
-                         SEXP numberOfCols = NULL,
-                         SEXP numberOfPlanes = NULL);
-
 class CRECATFile : public CECATFile
 {
 public:
   CRECATFile(const QString& filename,
              CECATMainHeader::Type fileType = CECATMainHeader::Unknown);
-
-  operator SEXP();
 
   bool readMainHeader(Rcpp::List& mainHeader);
   bool readMatrix(Rcpp::NumericVector*& matrixData, Rcpp::List& subHeader,
---
+++ src/rcpp/RInterface.h
@@ -0,0 +1,12 @@
+#ifndef RINTERFACE_H
+#define RINTERFACE_H
+
+#include <Rcpp.h>
+
+// this function can be called from R using the .Call interface
+RcppExport SEXP readEcat(SEXP vfile,
+                         SEXP numberOfVolumes = NULL,
+                         SEXP numberOfRows = NULL,
+                         SEXP numberOfCols = NULL,
+                         SEXP numberOfPlanes = NULL);
+#endif
