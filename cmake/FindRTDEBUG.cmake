fba0eeea3b6de904fff99b550e642aeffc0f8f39
Jens Maus <mail@jens-maus.de>
2020-09-17 10:30:06 +0000

    use NO_CMAKE_SYSTEM_PATH to ensure we only use our own libs from the supplied PREFIX_PATH and not from a system search path.

--- cmake/FindRTDEBUG.cmake
+++ cmake/FindRTDEBUG.cmake
@@ -7,28 +7,36 @@
 #  RTDEBUG_SHARED_LIB - List of shared libraries when using RTDEBUG
 #  RTDEBUG_DEFINITIONS - Compiler switches required for using RTDEBUG
 
-if(RTDEBUG_INCLUDES)
-  set(RTDEBUG_FIND_QUIETLY TRUE)
-endif(RTDEBUG_INCLUDES)
-  
-find_path(RTDEBUG_INCLUDES CRTDebug.h
-          PATH_SUFFIXES include/rtdebug
-)
-  
-find_library(RTDEBUG_STATIC_LIB NAMES librtdebug${CMAKE_STATIC_LIBRARY_SUFFIX}
-             PATH_SUFFIXES lib
-)
-find_library(RTDEBUG_SHARED_LIB NAMES librtdebug${CMAKE_SHARED_LIBRARY_SUFFIX} rtdebug
-             PATH_SUFFIXES lib
-)
- 
-# set the global DEBUG define while compiling
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DDEBUG")
+# only search for RTDEBUG in debug mode
+if(CMAKE_BUILD_TYPE MATCHES Debug)
 
-# handle the QUIETLY and REQUIRED arguments and set RTDEBUG_FOUND to TRUE if
-# all listed variables are TRUE
-include(FindPackageHandleStandardArgs)
-set(RTDEBUG_LIBS ${RTDEBUG_STATIC_LIB} ${RTDEBUG_SHARED_LIB})
-find_package_handle_standard_args(RTDEBUG DEFAULT_MSG RTDEBUG_LIBS RTDEBUG_INCLUDES)
+  if(RTDEBUG_INCLUDES)
+    set(RTDEBUG_FIND_QUIETLY TRUE)
+  endif(RTDEBUG_INCLUDES)
   
-mark_as_advanced(RTDEBUG_STATIC_LIB RTDEBUG_SHARED_LIB RTDEBUG_INCLUDES)
+  find_path(RTDEBUG_INCLUDES CRTDebug.h
+            PATH_SUFFIXES include/rtdebug
+            NO_CMAKE_SYSTEM_PATH
+  )
+  
+  find_library(RTDEBUG_STATIC_LIB NAMES librtdebug${CMAKE_STATIC_LIBRARY_SUFFIX}
+               PATH_SUFFIXES lib
+               NO_CMAKE_SYSTEM_PATH
+  )
+  find_library(RTDEBUG_SHARED_LIB NAMES librtdebug${CMAKE_SHARED_LIBRARY_SUFFIX} rtdebug
+               PATH_SUFFIXES lib
+               NO_CMAKE_SYSTEM_PATH
+  )
+ 
+  # set the global DEBUG define while compiling
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DDEBUG")
+
+  # handle the QUIETLY and REQUIRED arguments and set RTDEBUG_FOUND to TRUE if
+  # all listed variables are TRUE
+  include(FindPackageHandleStandardArgs)
+  set(RTDEBUG_LIBS ${RTDEBUG_STATIC_LIB} ${RTDEBUG_SHARED_LIB})
+  find_package_handle_standard_args(RTDEBUG DEFAULT_MSG RTDEBUG_LIBS RTDEBUG_INCLUDES)
+  
+  mark_as_advanced(RTDEBUG_STATIC_LIB RTDEBUG_SHARED_LIB RTDEBUG_INCLUDES)
+
+endif()
